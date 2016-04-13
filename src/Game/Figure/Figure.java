bca45813da9c57e77cef68e57e046174bcbdee09
bartlomey <bartx67@gmail.com>
2016-04-13 18:34:54 +0200

    dupa

--- src/Game/Figure/Figure.java
+++ src/Game/Figure/Figure.java
@@ -7,8 +7,9 @@ import java.util.List;
 /**
  * Created by dominikolczyk on 13.04.2016.
  */
-public interface Figure {
-	String color;
+public interface Figure
+{
+	Integer color;
 	Integer value;
 	List<Pair<Integer,Integer>> possibleMoves(Pair<Integer,Integer>);
 }
---
+++ src/Game/Figure/Pawn.java
@@ -0,0 +1,33 @@
+package Game.Figure;
+
+/**
+ * Created by ASUS on 2016-04-13.
+ */
+public class Pawn extends Figure
+{
+
+    boolean hasMoved;
+    Integer value = 1;
+
+
+    Pawn(Integer color_)
+    {
+        this.color = color_;
+        hasMoved = false;
+
+    }
+
+    if(hasMoved == false)
+    {
+        hasMoved = true;
+
+
+
+    }
+
+
+
+
+
+
+}
